661170fb19bf11770404a48d32f4c86c8278a594
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2023-01-10 22:59:26 +0000

    autenticação com o djongo

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cart', views.cart, name='cart'),
     path('checkout', views.checkout, name='checkout'),
-    path('produto', views.produto, name='produto')
+    path('produto', views.produto, name='produto'),
+    path('login/', views.UserLogin, name='login')
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse, Http404
 from django.template.response import TemplateResponse
 from store.models import Produtos, User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def index(request):
+    print("teste")
+    if (request.user.is_anonymous):
+        print("no login")
     if request.method == 'GET':
         prods = Produtos.objects.all()
         context = {"prods": prods}
@@ -18,5 +24,25 @@ def cart(request):
 def checkout(request):
     return render(request, "checkout.html")
 
+
 def produto(request):
     return render(request, 'produto.html')
+
+
+def UserLogin(request):
+    if request.method == "POST":
+        print("POST LOGIN")
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid Credentials!!!" + username)
+        alert = True
+        return render(request, 'login.html', {'alert': alert})
+    return render(request, "login.html")
