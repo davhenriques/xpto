c20cd6861c6a754b8cbd60fd9adbef3d1c75b0aa
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2023-01-25 01:55:31 +0000

    permissões + fixed register user_groups

--- store/views.py
+++ store/views.py
@@ -5,29 +5,52 @@ from store.models import Produtos
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 
 
-# Create your views here.
+# tests
+def isAdmin(user):
+    return 'admin' in list(user.groups.values_list('name', flat=True))
+
+
+def isComercial1(user):
+    print(list(user.groups.values_list('name', flat=True)))
+    return 'Comercial Administrador' in list(user.groups.values_list('name', flat=True))
+
+
+def isComercial2(user):
+    return 'Comercial Supervisionador' in list(user.groups.values_list('name', flat=True))
+
+
+def isParceiro(user):
+    return 'Parceiro' in list(user.groups.values_list('name', flat=True))
+
+
+def isComprador(user):
+    return 'Comprador' in list(user.groups.values_list('name', flat=True))
+
+
+
+
+# views
 def index(request):
-    print("teste")
-    if (request.user.is_anonymous):
-        print("no login")
     if request.method == 'GET':
         prods = Produtos.objects.all()
         context = {"prods": prods}
         return TemplateResponse(request, "index.html", context)
 
-
+@user_passes_test(isComercial1)
 def cart(request):
     return render(request, "cart.html")
 
-@permission_required('teste', login_url='/')
+
+@user_passes_test(isComprador)
 def checkout(request):
     return render(request, "checkout.html")
 
-
+@user_passes_test(lambda u : isComercial1(u) or isComercial2(u))
 def produto(request):
+    print("def produto")
     return render(request, 'produto.html')
 
 
@@ -49,12 +72,15 @@ def register(request):
                 messages.error(request, "email já associado a uma conta")
                 return redirect('/register')
             except User.DoesNotExist:
+                print("teste")
                 user = User.objects.create_user(request.POST['username'], request.POST['email'],
                                                 request.POST['password'])
                 group = Group.objects.get(name=request.POST['group'])
-
-
+                user.save()
+                group.user_set.add(user)
         return render(request, '../templates/registration/login.html')
     groups = Group.objects.all()
     context = {"groups": groups}
     return render(request, 'register.html', context)
+
+
